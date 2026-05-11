445ba736b8b4d9a7993bb5ecf1c407307ba7135d
abujafor1924 <abujaforwd@yahoo.com>
2026-05-11 17:40:29 +0600

    add hospital details id

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Appointment, Payment
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -24,3 +24,23 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('phone_number', 'name', 'email')
     ordering = ('-created_at',)
     readonly_fields = ('created_at', 'updated_at')
+
+class PaymentInline(admin.StackedInline):
+    model = Payment
+    extra = 0
+    can_delete = False
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'patient_name', 'patient_phone', 'service_type', 'appointment_date', 'appointment_time', 'status', 'user')
+    list_filter = ('status', 'service_type', 'appointment_date')
+    search_fields = ('patient_name', 'patient_phone', 'user__phone_number')
+    inlines = [PaymentInline]
+    readonly_fields = ('created_at',)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'appointment', 'transaction_id', 'amount', 'method', 'status', 'created_at')
+    list_filter = ('status', 'method', 'created_at')
+    search_fields = ('transaction_id', 'appointment__patient_name')
+    readonly_fields = ('created_at',)
